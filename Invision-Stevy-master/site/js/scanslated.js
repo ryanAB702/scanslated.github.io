2d40ee099aad6a34c0a58109695ebec095637903
law826 <law826@gmail.com>
2016-11-12 18:42:41 -0500

    upload stevy site

---
+++ Invision-Stevy-master/site/js/scanslated.js
@@ -0,0 +1,67 @@
+// Freelancer Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+})(jQuery); // End of use strict
+
+// Scroll to target
+function scrollToTarget(D)
+{
+    if(D == 1) // Top of page
+    {
+        D = 0;
+    }
+    else if(D == 2) // Bottom of page
+    {
+        D = $(document).height();
+    }
+    else // Specific Bloc
+    {
+        D = $(D).offset().top;
+        if($('.sticky-nav').length) // Sticky Nav in use
+        {
+            D = D-100;
+        }
+    }
+
+    $('html,body').animate({scrollTop:D}, 'slow');
+}
