3a56bc30f551f2299ab63aa6cf1f1906b35094ce
Estlin Haiss <eph5@duke.edu>
2017-06-13 15:27:06 -0400

    fixed synchronized highlighting on breast diagram

--- prototypes/js/breast-diagram-highlight.js
+++ prototypes/js/breast-diagram-highlight.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-
-  $('.yellow-fill').hover(function(){
-    $('.yellow-fill').toggleClass('highlight');
-    // $('.region-label p').text($('.solid-button.yellow-fill').data('region-name'));
-    // $('.region-label').toggleClass('show-label');
-  });
-  $('.green-fill').hover(function(){
-    $('.green-fill').toggleClass('highlight');
-    // $('.region-label p').text($('.solid-button.green-fill').data('region-name'));
-    // $('.region-label').toggleClass('show-label');
+  
+  $('#summary *[data-finding]').hover(function(){
+    //  console.log($(this).data('finding'));
+     var currentFinding = $(this).data('finding');
+    $('#summary *[data-finding=' + currentFinding + ']').toggleClass('highlight');
   });
 
 });
